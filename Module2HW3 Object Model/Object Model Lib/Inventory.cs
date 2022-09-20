377c6371b0a9c7038841ec2240f0eddd5c06a450
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2022-09-20 19:10:51 +0300

    112

---
+++ Module2HW3 Object Model/Object Model Lib/Inventory.cs
@@ -0,0 +1,87 @@
+﻿using Object_Model_Lib;
+
+namespace Module2HW3_Object_Model
+{
+    public class Inventory
+    {
+        private readonly AllWeaponTypes _types;
+        private Weapon[] _weapons;
+        private static Inventory _instance;
+        private Inventory(int count)
+        {
+            _weapons = new Weapon[count];
+            AddRandomWeaponToInventory();
+        }
+
+        public static int SetInstance
+        {
+            set
+            {
+                if (_instance == null)
+                {
+                    _instance = new Inventory(value);
+                }
+            }
+        }
+
+        public static Inventory GetInstance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public Weapon[] Bag
+        {
+            get
+            {
+                return _weapons;
+            }
+        }
+
+        public void AddRandomWeaponToInventory()
+        {
+            for (int i = 0; i < _weapons.Length; i++)
+            {
+                _weapons[i] = WeaponSpawner.Spawner((AllWeaponTypes)new Random().Next(0, 5));
+            }
+        }
+
+        public Weapon AddToInventory()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Индекс оружия для спавна");
+            Console.WriteLine("1 - Shield, 2 - Sword, 3 - Bow, 4 - Crossbow, 5 - Rifle, 6 - AutoRifle");
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.D1:
+                    return WeaponSpawner.Spawner(AllWeaponTypes.Shield);
+                case ConsoleKey.D2:
+                    return WeaponSpawner.Spawner(AllWeaponTypes.Sword);
+                case ConsoleKey.D3:
+                    return WeaponSpawner.Spawner(AllWeaponTypes.Bow);
+                case ConsoleKey.D4:
+                    return WeaponSpawner.Spawner(AllWeaponTypes.Crossbow);
+                case ConsoleKey.D5:
+                    return WeaponSpawner.Spawner(AllWeaponTypes.Rifle);
+                case ConsoleKey.D6:
+                    return WeaponSpawner.Spawner(AllWeaponTypes.AutoRifle);
+                default:
+                    Console.WriteLine("Ничего не заспавнено...");
+                    return null;
+            }
+        }
+
+        public void RemoveWeaponFromInventory(int index)
+        {
+            if (index < _weapons.Length)
+            {
+                if (_weapons[index] != null)
+                {
+                    _weapons[index] = null;
+                }
+            }
+        }
+    }
+}
---
+++ Module2HW3 Object Model/Object Model Lib/Weapons/TypeWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Model_Lib
+{
+    /// <summary>
+    /// Тип оружия: ближний или дальний бой
+    /// </summary>
+    public enum TypeWeapon
+    {
+        MeleeDamage,
+        RangeDamage
+    }
+}
